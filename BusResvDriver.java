a2b86eee56d37ff27a1e5465b69b4c0252205378
Sivnaveen L. <sivanaveenecemec@gmail.com>
2024-08-09 20:49:30 +0530

    Update BusResvDriver.java

--- BusResvDriver.java
+++ BusResvDriver.java
@@ -1,45 +1,38 @@
 package BusRev;
 
-
-import java.util.ArrayList;
-import java.util.Scanner;
-public class BusResvDriver {
-	public static void main(String[] args) {
-		
-		ArrayList<Bus>buses=new ArrayList<Bus>();
-		buses.add(new Bus(1,true,2));
-		buses.add(new Bus(2,false,55));
-		buses.add(new Bus(3,true,56));
-		
-		ArrayList<Booking>bookings=new ArrayList<Booking>();
-		
-		
-		
-		int userOpt=1;
-		Scanner sc=new Scanner(System.in);
-		
-		for(Bus b:buses) {
-			b.displayBusInfo();
-		}
-		
-		
-		
-		while(userOpt==1) {
-			System.out.println("Enter 1 to Booking");
-			System.out.println("Enter 2 to Exit");
-			userOpt=sc.nextInt();
-			if(userOpt==1) {
-				Booking booking=new Booking();
-				if(booking.isAvailable(bookings,buses))
-				{
-					bookings.add(booking);
-			        System.out.println("Your Booking is Confirmed");
-					
-				}
-				else
-					System.out.println("Sorry Bus is Fulled,Try Another Bus");
-			}
-		}
+public class Bus {
+	private int busNo;
+	private boolean ac;
+	private int capacity;
+	
+	public int getBusNO() {
+		return busNo;
 	}
+	public boolean isAc() {
+		return ac;
+	}
+
+	public void setAc(boolean ac) {
+		this.ac = ac;
+	}
+
+	public int getCapacity() {
+		return capacity;
+	}
+
+	public void setCapacity(int capacity) {
+		this.capacity = capacity;
+	}
+
+	public Bus(int busNo, boolean ac, int capacity) {
+		super();
+		this.busNo = busNo;
+		this.ac = ac;
+		this.capacity = capacity;
+	}
+	
+	public void displayBusInfo(){
+		 System.out.println("Bus No:"+busNo+"   Ac:"+ac+"   Total Capacity:"+capacity);
+	 }
 
 }
